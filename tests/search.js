6f636d1695bf66877a5b27db5f4d0dcecac1aff6
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2023-04-25 11:25:32 -0500

    DS Store

--- tests/search.js
+++ tests/search.js
@@ -7,7 +7,7 @@ export async function search() {
   const page = await browser.newPage();
 
   await page.goto(
-    "https://www.linkedin.com/jobs/search?keywords=Software%20developer&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0"
+    "https://www.linkedin.com/jobs/search?keywords=Software%20Developer&location=Houston%2C%20Texas%2C%20United%20States&geoId=103743442&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0"
   );
   await wait(5000);
 }
